76313cb0d44e1bf5177a4c4fc372e07b3e61f0cd
unknown <14928811+Username1111111111@users.noreply.github.com>
2024-02-27 17:54:49 +0400

    Initial upload for deploy

--- app/page.js
+++ app/page.js
@@ -1,6 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+
+// Test
+if (process.env.NODE_ENV === 'dev') {
+  console.log('dev mode');
+} else {
+  console.log('prod mode');
+}
+
 export default function Home() {
   return (
     <main className={styles.main}>
