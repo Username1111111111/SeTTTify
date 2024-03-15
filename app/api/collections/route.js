b5032aca52be8ed1fedbab2123fb9dd42a42f25f
Username1111111111 <14928811+Username1111111111@users.noreply.github.com>
2024-03-15 22:01:14 +0400

    Prisma path fix.

--- app/api/collections/route.js
+++ app/api/collections/route.js
@@ -1,4 +1,4 @@
-import prisma from '@lib/prisma';
+import prisma from '@/lib/prisma';
 
 async function handler(req) {
     if (req.method === "GET") {
--- app/api/topic/route.js
+++ app/api/topic/route.js
@@ -1,4 +1,5 @@
-import prisma from '@lib/prisma';
+import prisma from '@/lib/prisma';
+
 
 async function handler(req) {
     if (req.method === "GET") {
