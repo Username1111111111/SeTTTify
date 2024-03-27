3154eb46bf1a32f48516f1fb5745fb9b662807cc
Username1111111111 <14928811+Username1111111111@users.noreply.github.com>
2024-03-27 21:40:53 +0400

    Added image placeholder, optimized svg size

--- ui/collections/fields/imageField.js
+++ ui/collections/fields/imageField.js
@@ -2,16 +2,16 @@ import Image from "next/image";
 import styles from "./imageField.module.css";
 
 export default function ImageField({ collectionImageUrl }) {
-// export default function ImageField({ }) {
 
-    // let collectionImageUrl = 'lu9xezbk31cq5-soywojak.png';
+    const placeholderImageUrl = '/placeholder.png';
+    const imageUrl = collectionImageUrl ? `https://storage.googleapis.com/setttify/${collectionImageUrl}` : placeholderImageUrl;
 
     return (
         <li className="row mb-1 w-100 p-0 m-0 d-flex flex-column justify-content-start align-items-center">
             <div className={`col-6 p-0 m-0 ${styles.imageContainer}`}>
                 <Image
                     alt="Cat"
-                    src={`https://storage.googleapis.com/setttify/${collectionImageUrl}`}
+                    src={imageUrl}
                     width={100}
                     height={100}
                     style={{
