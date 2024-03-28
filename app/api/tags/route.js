962845dee43cf4730bad53f685f88c204ff6aa93
Username1111111111 <14928811+Username1111111111@users.noreply.github.com>
2024-03-28 21:12:13 +0400

    Tags autocomplete, suggestions have been implemented along with react-tag-input

--- app/api/tags/route.js
+++ app/api/tags/route.js
@@ -1,57 +1,79 @@
 import prisma from "@/lib/prisma";
+import catchResponse from "@/lib/catchResponse";
 
 async function handler(req) {
+    const searchParams = req.nextUrl.searchParams;
     if (req.method === "GET") {
-        try {
-
-            const tags = await prisma.tag.findMany({
-                take: 10,
-                select: {
-                    id: true,
-                    name: true,
-                    items: {
-                        // orderBy: {
-                        //     items: {
-                        //         _count: "desc",
-                        //     },
-                        // },
-                        select: {
-                            id: true,
-                            name: true
-                        },
-                        take: 5,
+        if (searchParams.has("all")) {
+            // const all = searchParams.get("all");
+            try {
+                const tags = await prisma.tag.findMany({
+                    select: {
+                        name: true,
                     },
-                    _count: {
-                        select: {
-                            items: true,
+                });
+
+                const resBody = JSON.stringify(tags);
+
+                const res = new Response(resBody, {
+                    status: 200,
+                    statusText: "Tags have been fetched",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+
+                return res;
+            } catch (error) {
+                const message = `Failed to fetch all tags`;
+                const res = catchResponse(error, message);
+
+                return res;
+            }
+        } else {
+            try {
+                const tags = await prisma.tag.findMany({
+                    take: 10,
+                    select: {
+                        id: true,
+                        name: true,
+                        items: {
+                            // orderBy: {
+                            //     items: {
+                            //         _count: "desc",
+                            //     },
+                            // },
+                            select: {
+                                id: true,
+                                name: true,
+                            },
+                            take: 5,
+                        },
+                        _count: {
+                            select: {
+                                items: true,
+                            },
                         },
                     },
-                },
-            });
+                });
 
-            const resBody = JSON.stringify(tags);
+                const resBody = JSON.stringify(tags);
 
-            const res = new Response(resBody, {
-                status: 200,
-                statusText: "Tags have been fetched",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
+                const res = new Response(resBody, {
+                    status: 200,
+                    statusText: "Tags have been fetched",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
 
-            return res;
-        } catch (error) {
-            const resBody = JSON.stringify({ error: error.message });
+                return res;
+            } catch (error) {
+                const message = `Failed to fetch some tags`;
+                const res = catchResponse(error, message);
 
-            const res = new Response(resBody, {
-                status: 500,
-                statusText: `Failed to fetch tags`,
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
-
-            return res;
+                return res;
+            }
         }
     }
 }
--- ui/items/inputs/tagInput.js
+++ ui/items/inputs/tagInput.js
@@ -1,80 +1,127 @@
 "use client";
-import { useState } from "react";
-import { IoMdClose } from "react-icons/io";
-import styles from "./tagInput.module.css";
-// import generateUniqueId from "@/lib/generateUniqueId";
-import { useEffect } from "react";
-import { useTranslations } from "next-intl";
+import React, { useEffect, useState } from "react";
+import { WithContext as ReactTags } from "react-tag-input";
+import getTagsAll from "@/lib/getTagsAll";
+// import styles from './YourStyles.module.css';
 
-export default function TagInput({ itemId, placeholder, name, value, onChange }) {
-    const [tags, setTags] = useState([]);
+const TagInput = ({ itemId, value, onChange, placeholder, name }) => {
     const [inputValue, setInputValue] = useState("");
-    const t = useTranslations("Item")
+    const [tags, setTags] = useState([]);
+    const [suggestions, setSuggestions] = useState([]);
+
+    async function fetchSuggestions() {
+        const fetchedSuggestions = await getTagsAll();
+        console.log(fetchedSuggestions);
+        return fetchedSuggestions;
+    }
 
     useEffect(() => {
-        setTags(value);
+        fetchSuggestions().then((fetchedSuggestions) => {
+            setSuggestions(
+                fetchedSuggestions.map((suggestion) => ({
+                    id: suggestion.name,
+                    text: suggestion.name,
+                }))
+            );
+        });
+    }, []);
+
+    useEffect(() => {
+        setTags(
+            value.map((tag, index) => ({ id: index.toString(), text: tag }))
+        );
+        // console.log(value);
+        // console.log(tags);
     }, [value]);
 
-    const handleInputChange = (e) => {
-        setInputValue(e.target.value);
+    const handleDelete = (i) => {
+        let newTags = tags.filter((tag, index) => index !== i);
+        setTags(newTags);
+
+        let newTagsArr = [];
+        newTags.map((tag) => newTagsArr.push(tag.text));
+        onChange(newTagsArr);
     };
 
-    const handleInputKeyDown = (e) => {
-        if (e.key === "Enter" && inputValue) {
-            if (!tags.includes(inputValue)) {
-                setTags([...tags, inputValue]);
-                // console.log([...tags, inputValue])
-                onChange([...tags, inputValue]);
-                setInputValue("");
-            } else {
-                alert(`${t("duplicated_tag")}`);
-            }
+    const handleAddition = (tag) => {
+        if (typeof tags !== "string") {
+            setTags([...tags, tag]);
+
+            let prevTags = [];
+            tags.map((tag) => prevTags.push(tag.text));
+            onChange([...prevTags, tag.text]);
+        } else {
+            setTags([tag]);
+            onChange([tag.text]);
         }
     };
 
-    const removeTag = (indexToRemove) => {
-        const newTags = tags.filter((_, index) => index !== indexToRemove);
+    const handleDrag = (tag, currPos, newPos) => {
+        const newTags = [...tags].slice();
+        newTags.splice(currPos, 1);
+        newTags.splice(newPos, 0, tag);
         setTags(newTags);
-        // console.log(newTags)
-        onChange(newTags);
+
+        let newTagsArr = [];
+        newTags.map((tag) => newTagsArr.push(tag.text));
+        onChange(newTagsArr);
     };
 
-    // const inputId = `tags-${itemId ? itemId : "create"}-${generateUniqueId()}`;
-    const inputId = `tags-${itemId ? itemId : "create"}`;
+    const handleTagClick = (index) => {
+        // console.log("The tag at index " + index + " was clicked");
+    };
+
+    const handleInputChange = (input) => {
+        setInputValue(input);
+    };
 
     return (
         <li className="row w-100 p-0 m-0 d-flex flex-row justify-content-start align-items-center mb-2">
             <div className="col-5 col-md-4 m-0 p-0">{name}:</div>
             <div className="col-7 col-md-8 pe-0">
-                <div className="form-control border border-secondary p-1 m-0 d-flex flex-wrap justify-content-start align-items-center">
-                    {tags.map((tag, index) => (
-                        <span
-                            key={index}
-                            className="btn-primary border border-secondary rounded d-flex flex-row justify-content-start align-items-center m-1 ps-1 text-nowrap"
-                        >
-                            {tag}
-                            <button
-                                type="button"
-                                className="btn d-flex flex-row justify-content-start align-items-center p-0 m-0 ms-1 me-1 bg-body"
-                                onClick={() => removeTag(index)}
-                            >
-                                <IoMdClose color={"red"} />
-                            </button>
-                        </span>
-                    ))}
-
-                    <input
-                        type="text"
-                        value={inputValue}
-                        id={inputId}
-                        onChange={handleInputChange}
-                        onKeyDown={handleInputKeyDown}
-                        placeholder={`${placeholder}...`}
-                        size={6}
-                        className={`${styles.tagInput} bg-body border border-secondary rounded p-0 ps-2 pe-2 m-0`}
+                <div className="bg-body-secondary p-0 m-0 d-flex justify-content-center align-items-center">
+                    <ReactTags
+                        tags={tags}
+                        suggestions={suggestions}
+                        handleDelete={handleDelete}
+                        handleAddition={handleAddition}
+                        handleDrag={handleDrag}
+                        handleInputChange={handleInputChange}
+                        handleTagClick={handleTagClick}
+                        minQueryLength={1}
+                        inputValue={inputValue}
+                        placeholder={placeholder}
+                        delimiters={delimiters}
+                        maxLength={8}
+                        allowUnique={true}
+                        allowDeleteFromEmptyInput={false}
+                        classNames={{
+                            tags: "w-100",
+                            tag: "border border-secondary rounded m-1 ps-1 pe-1 text-nowrap",
+                            remove: "p-0 m-0 ms-1 bg-transparent border-0",
+                            tagInput: "w-100",
+                            tagInputField:
+                                "w-100 form-control border border-secondary mb-2 mt-2",
+                            selected: "m-0 p-0",
+                            suggestions:
+                                "btn d-flex flex-row text-center m-0 p-0 bg-transparent",
+                            // activeSuggestion: "text-center d-flex flex-column justify-content-start align-items-center",
+                            // editTagInput: "editTagInputClass",
+                            // editTagInputField: "",
+                            // clearAll: "",
+                        }}
                     />
                 </div>
             </div>
         </li>
     );
-}
+};
+
+const KeyCodes = {
+    comma: 188,
+    enter: 13,
+};
+
+const delimiters = [KeyCodes.comma, KeyCodes.enter];
+
+export default TagInput;
